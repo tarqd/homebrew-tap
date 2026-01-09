5de655ea64a0c7e6c2a94c707aecdf287a141998
Christopher Tarquini <ctarquini@launchdarkly.com>
2026-01-09 17:43:06 -0500

    Remove redundant version field

--- Formula/mdcopy.rb
+++ Formula/mdcopy.rb
@@ -1,27 +1,26 @@
 class Mdcopy < Formula
   desc "Convert markdown to clipboard with rich text, HTML, and RTF formats"
   homepage "https://github.com/tarqd/mdcopy"
-  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
-      url "https://github.com/tarqd/mdcopy/releases/download/v#{version}/mdcopy-aarch64-apple-darwin.tar.gz"
+      url "https://github.com/tarqd/mdcopy/releases/download/v0.1.1/mdcopy-aarch64-apple-darwin.tar.gz"
       sha256 "d64e4b6ea0f4674856f454a34aeee098c540312e98acb79209e7d816f9e89d61"
     end
     on_intel do
-      url "https://github.com/tarqd/mdcopy/releases/download/v#{version}/mdcopy-x86_64-apple-darwin.tar.gz"
+      url "https://github.com/tarqd/mdcopy/releases/download/v0.1.1/mdcopy-x86_64-apple-darwin.tar.gz"
       sha256 "43776d2251303212e88d0701fba7de328246a95d46648851e58beaccf8c16ea9"
     end
   end
 
   on_linux do
     on_arm do
-      url "https://github.com/tarqd/mdcopy/releases/download/v#{version}/mdcopy-aarch64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/tarqd/mdcopy/releases/download/v0.1.1/mdcopy-aarch64-unknown-linux-gnu.tar.gz"
       sha256 "66bb7d3a083058cbddc9b602f5ee4ec86ff3144780fd7e677395c9dd3671396b"
     end
     on_intel do
-      url "https://github.com/tarqd/mdcopy/releases/download/v#{version}/mdcopy-x86_64-unknown-linux-gnu.tar.gz"
+      url "https://github.com/tarqd/mdcopy/releases/download/v0.1.1/mdcopy-x86_64-unknown-linux-gnu.tar.gz"
       sha256 "97d37a870b42552c53562e1da61c87b5070a340f856f9a72d8d21134341c34cb"
     end
   end
